691fb4270a2de005725e5cd6d0d196a8c3347018
Abbas <abbas.ajorlou1371@gmail.com>
2023-12-30 10:27:55 +0330

    Fix category option indentation and display parent categories in select dropdown

--- resources/views/components/partials/category-option.blade.php
+++ resources/views/components/partials/category-option.blade.php
@@ -1,7 +1,7 @@
 
 @props(['category', 'level' => 0])
 
-<option value="{{ $category->id }}">{{ str_repeat('--', $level) . $category->name }}</option>
+<option value="{{ $category->id }}">{{ str_repeat('-', $level) . $category->name }}</option>
 @if ($category->children)
     @foreach ($category->children as $child)
         <x-partials.category-option :category="$child" :level="$level + 1" />
