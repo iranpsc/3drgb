139cc1a072749667b713f79cc72a7a92fb3d43c4
abbass <abbas.ajorlou1371@gmail.com>
2024-03-26 20:57:04 +0330

    Small bug fix

--- resources/views/components/form/text.blade.php
+++ resources/views/components/form/text.blade.php
@@ -5,7 +5,7 @@
     <div class="flex flex-col gap-5">
         <input type="{{ $type }}" class="form-control @error($name) is-invalid @enderror w-full  bg-[#F8F9FA] dark:bg-[#4A4E7C] rounded-[10px] p-4 " name="{{ $name }}" id="{{ $name }}" {{ $attributes }}>
         @error($name)
-            <span <span style="color:red;padding:14px;background-color:rgba(207, 117, 117, 0.47);border-radius:10px">{{ $message }}</span>
+            <span style="color:red;padding:14px;background-color:rgba(207, 117, 117, 0.47);border-radius:10px">{{ $message }}</span>
         @enderror
     </div>
 </div>
