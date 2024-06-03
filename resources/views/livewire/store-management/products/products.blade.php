0a3fa76f2e2c8c74364ccf61d46e002e05375b86
abbass <abbas.ajorlou1371@gmail.com>
2024-06-03 09:31:51 +0330

    refactor: Remove unnecessary conditionals in products.blade.php

--- resources/views/livewire/store-management/products/products.blade.php
+++ resources/views/livewire/store-management/products/products.blade.php
@@ -65,14 +65,8 @@
                             <div class="userDatatable-content flex items-center ">
                                 <div class="d-flex justify-content-start gap-2">
                                     <a href="{{ route('products.edit', $product->id) }}" class="px-2 py-1 rounded-[10px] text-white font-bold text-sm" style="background-color:#0EBDE2;">ویرایش</a>
-
-                                    @if ($product->published)
-                                        <a href="{{ route('products.show', $product->sku) }}" class="px-2 py-1 rounded-[10px] text-white font-bold text-sm" style="background-color:blue">جزییات</a>
-                                    @endif
-
-                                    @if(!$product->hasOrders())
-                                        <button type="button" class="px-2 py-1 rounded-[10px] text-white font-bold text-sm" wire:click="delete({{ $product->id }})" wire:confirm="آیا از حذف این محصول مطمئن هستید؟" style="background-color:red;">حذف</button>
-                                    @endif
+                                    <a href="{{ route('products.show', $product->sku) }}" class="px-2 py-1 rounded-[10px] text-white font-bold text-sm" style="background-color:blue">جزییات</a>
+                                    <button type="button" class="px-2 py-1 rounded-[10px] text-white font-bold text-sm" wire:click="delete({{ $product->id }})" wire:confirm="آیا از حذف این محصول مطمئن هستید؟" style="background-color:red;">حذف</button>
                                 </div>
                             </div>
                         </td>
