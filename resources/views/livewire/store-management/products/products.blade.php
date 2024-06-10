ce0336799c25cc63f88c3e3e5d37715a05f808dc
theAwmin <awmin.dn@gmail.com>
2024-06-10 12:22:25 +0330

    fix input search

--- resources/views/livewire/store-management/products/products.blade.php
+++ resources/views/livewire/store-management/products/products.blade.php
@@ -1,8 +1,23 @@
 <div>
     <x-page title="محصولات">
 
-        <input type="text" wire:model.live="search">
+        
+        <div class="w-full flex gap-5  mt-5 bg-[#D8E5FD] dark:bg-[#4A4E7C] rounded-full  p-2  " style="max-width: 320px ;margin-bottom: 20px">
+            <div class="flex justify-center items-center p-2 w-min">
+                <svg width="28" height="28" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path class="dark:stroke-white"
+                        d="M11.4582 21.7501C17.1421 21.7501 21.7498 17.1423 21.7498 11.4584C21.7498 5.77448 17.1421 1.16675 11.4582 1.16675C5.77424 1.16675 1.1665 5.77448 1.1665 11.4584C1.1665 17.1423 5.77424 21.7501 11.4582 21.7501Z"
+                        stroke="#000BEE" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path class="dark:stroke-white" d="M22.8332 22.8334L20.6665 20.6667"
+                        stroke="#000BEE" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                </svg>
+            </div>
 
+                <input placeholder="جستوجو محصول" type="text" class=" w-full  text-[#ACB9FA] font-bold ring-transparent outline-transparent    focus:!outline-0  focus:!right-0 border-0 focus:border-0 ring-offset-0  focus:ring-transparent     bg-transparent" wire:model.live="search">
+        </div>
         @session('message')
             <x-alert type="success" message="{{ session('message') }}" />
         @endsession
